e94ddda02f175535792cb6057e5e7a77ec92ed40
BernardByrnes <bernardtamboedu@gmail.com>
2025-12-10 16:58:19 +0300

    updated

--- 02-tours/starter/src/App.jsx
+++ 02-tours/starter/src/App.jsx
@@ -36,6 +36,25 @@ const App = () => {
       </main>
     );
   }
+
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>no tours Left</h2>
+          <button
+            type="button"
+            style={{ marginTop: "2rem" }}
+            className="btn"
+            onClick={fetchTours}
+          >
+            Refresh
+          </button>
+        </div>
+      </main>
+    );
+  }
+
   return (
     <main>
       <Tours tours={tours} removeTour={removeTour} />
