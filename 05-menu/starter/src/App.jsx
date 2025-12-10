4bacf486591586c72ad7e7d2efc10bf5fd404ff1
BernardByrnes <bernardtamboedu@gmail.com>
2025-12-11 00:35:27 +0300

    update

--- 05-menu/starter/src/App.jsx
+++ 05-menu/starter/src/App.jsx
@@ -1,4 +1,35 @@
+import { useState } from "react";
+import menu from "./data";
+import Title from "./Title";
+import Menu from "./Menu";
+import Categories from "./Categories";
+
+// const tempCategories = menu.map((item) => item.category);
+// console.log(tempCategories);
+// const tempSet = new Set(tempCategories);
+// console.log(tempSet);
+// const tempItems = ["all", ...tempSet];
+// console.log(tempItems);
+
+const allCategories = ["all", ...new Set(menu.map((item) => item.category))];
 const App = () => {
-  return <h2>Menu Starter</h2>;
+  const [menuItems, setMenuItems] = useState(menu);
+  const [categories, setCategories] = useState(allCategories);
+  const filterItems = (category) => {
+    if (category === "all") {
+      return setMenuItems(menu);
+    }
+    const newItems = menu.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+  return (
+    <main>
+      <section className="menu">
+        <Title text="Our Menu" />
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
 };
 export default App;
