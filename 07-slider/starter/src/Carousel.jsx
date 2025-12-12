7a4e0c456cfcc6b6b7fccd32f90f4793c69b4313
BernardByrnes <bernardtamboedu@gmail.com>
2025-12-12 22:16:32 +0300

    updated

--- 07-slider/starter/src/Carousel.jsx
+++ 07-slider/starter/src/Carousel.jsx
@@ -5,16 +5,21 @@ import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 
 const Carousel = () => {
   const [people, setPeople] = useState(list);
+  const [currentPerson, setCurrentPerson] = useState(1);
 
   const prevSlide = () => {};
   const nextSlide = () => {};
 
   return (
     <section className="slider-container">
-      {people.map((person) => {
+      {people.map((person, personIndex) => {
         const { id, image, name, title, quote } = person;
         return (
-          <article className="slide" key={id}>
+          <article
+            className="slide"
+            style={{ transform: `translateX ${100 * personIndex}%` }}
+            key={id}
+          >
             <img src={image} alt={name} className="person-img" />
             <h5 className="name">{name}</h5>
             <p className="title">{title}</p>
@@ -25,6 +30,9 @@ const Carousel = () => {
       })}
       <button type="button" className="prev" onClick={prevSlide}>
         <FiChevronLeft />
+      </button>
+      <button type="button" className="next" onClick={nextSlide}>
+        <FiChevronRight />
       </button>
       <button type="button" className="next" onClick={nextSlide}>
         <FiChevronRight />
