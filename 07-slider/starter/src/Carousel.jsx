a3f8ef60f26ce9d47ee5bc9dcdd39570ad6503ab
BernardByrnes <bernardtamboedu@gmail.com>
2025-12-13 20:58:21 +0300

    updated git commit -m updated git commit -m updated

--- 07-slider/starter/src/Carousel.jsx
+++ 07-slider/starter/src/Carousel.jsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
-import { list } from "./data";
+import { useEffect, useState } from "react";
+import { list, longList, shortList } from "./data";
 import { FaQuoteRight } from "react-icons/fa";
 import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 
 const Carousel = () => {
-  const [people, setPeople] = useState(list);
+  const [people, setPeople] = useState(longList);
   const [currentPerson, setCurrentPerson] = useState(0);
 
   const prevSlide = () => {
@@ -19,6 +19,15 @@ const Carousel = () => {
       return result;
     });
   };
+
+  useEffect(() => {
+    let sliderID = setInterval(() => {
+      nextSlide();
+    }, 2000);
+    return () => {
+      clearInterval(sliderID);
+    };
+  }, [currentPerson]);
 
   return (
     <section className="slider-container">
