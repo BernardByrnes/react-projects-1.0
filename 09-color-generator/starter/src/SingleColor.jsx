8a997a0ebf00fc87efce35714233d7562b0c3415
BernardByrnes <bernardtamboedu@gmail.com>
2025-12-14 19:57:09 +0300

    updated

--- 09-color-generator/starter/src/SingleColor.jsx
+++ 09-color-generator/starter/src/SingleColor.jsx
@@ -1,9 +1,24 @@
+import { toast } from "react-toastify";
+
 const SingleColor = ({ index, color }) => {
   const { hex, weight } = color;
+  const saveToClipboard = async () => {
+    if (navigator.clipboard) {
+      try {
+        await navigator.clipboard.writeText(`#${hex}`);
+        toast.success("Color Copied To Clipboard");
+      } catch (error) {
+        toast.error("Failed To Copy To Clipboard");
+      }
+    } else {
+      toast.error("Clipboard Access Not Available");
+    }
+  };
   return (
     <article
       className={index > 10 ? "color color-light" : "color"}
       style={{ backgroundColor: `#${hex}` }}
+      onClick={saveToClipboard}
     >
       <p className="percent-value">{weight}%</p>
       <p className="color-value">#{hex}</p>
