e1ef50067a9f5768769bf9c9586d96a3b53ccdc1
BernardByrnes <bernardtamboedu@gmail.com>
2025-12-15 22:44:18 +0300

    updated

---
+++ 12-sidebar/starter/src/Context.jsx
@@ -0,0 +1,10 @@
+import { createContext, useState, useContext } from "react";
+
+const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  return <AppContext.Provider>{children}</AppContext.Provider>;
+};
