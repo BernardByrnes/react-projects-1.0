392be8bd9dc2f8ec2b47a13d4f5f4c4fd94db384
BernardByrnes <bernardtamboedu@gmail.com>
2026-01-16 22:15:04 +0300

    updated

--- 30-react-projects/src/projects/02-randomize-colors/RandomizeColor.jsx
+++ 30-react-projects/src/projects/02-randomize-colors/RandomizeColor.jsx
@@ -1,10 +1,41 @@
 import Title from "../components/Title";
 
 const RandomizeColor = () => {
+  // function handleClick(e) {
+  //   console.log("click");
+  // }
+  const handleClick = (e) => {
+    console.log("click");
+    console.log(getRandomColor());
+    let body = document.querySelector("body");
+    body.style.background = getRandomColor();
+    e.target.style.background = getRandomColor();
+  };
+
+  function getRandomColor() {
+    let letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
   return (
-    <div>
-      <Title />
-      RandomizeColor
+    <div className="container m-auto text-center">
+      <Title text={"Randomize Color"} classes={"mb-4"} />
+      <button className="btn btn-danger" onClick={handleClick}>
+        Click Me
+      </button>
+      <button className="btn btn-success" onClick={handleClick}>
+        Click Me
+      </button>
+      <button className="btn btn-primary" onClick={handleClick}>
+        Click Me
+      </button>
+      <button className="btn btn-warning" onClick={handleClick}>
+        Click Me
+      </button>
     </div>
   );
 };
